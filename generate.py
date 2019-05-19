7984978139b74dc6f91ea2aa70f8211523071954
Chris Cooper <chris@coopernetics.com>
2019-05-19 02:26:43 -0400

    more fonts

--- generate.py
+++ generate.py
@@ -1,25 +1,47 @@
 import os
+import random
+import shutil
 
 from itertools import chain
 from PIL import ImageFont, ImageDraw, Image
 
-fonts = [
-    "Lato",
-    "Great_Vibes",
-    "Lobster_Two",
-    "Lora",
-    "VT323"
-]
+fonts = [name for name in os.listdir("fonts") if os.path.isdir(f'fonts/{name}')]
 
-for font_name in fonts:
-    font_path = f"fonts/{font_name}/{font_name.replace('_', '')}-Regular.ttf"
-    font = ImageFont.truetype(font_path, 50)
-    os.makedirs(f"images/{font_name}", exist_ok=True)
+charset = list(chain(range(ord("a"), ord("z")), range(ord("A"), ord("Z")), range(ord("0"), ord("9"))))
 
-    for character in chain(range(ord("a"), ord("z")), range(ord("A"), ord("Z")), range(ord("0"), ord("9"))):
-        image = Image.new('RGB', (100, 100), (255, 255, 255))
-        draw = ImageDraw.Draw(image)
+def get_random_char():
+    return chr(random.choice(charset))
 
-        draw.text((25, 25), chr(character), font=font, fill=(0, 0, 0))
+def get_random_word():
+    length = random.randint(1, 7)
+    return "".join([get_random_char() for i in range(length)])
 
-        image.save(f"images/{font_name}/{character}.png")
+def get_random_string():
+    word_count = random.randint(1, 6)
+    return " ".join([get_random_word() for i in range(word_count)])
+
+
+if __name__ == '__main__':
+    for font_name in fonts:
+        font_path = f"fonts/{font_name}/{font_name.replace('_', '')}-Regular.ttf"
+        print(f'Loading {font_path}')
+        font = ImageFont.truetype(font_path, 25)
+        dir_path = f"images/{font_name}"
+        if os.path.exists(dir_path):
+            shutil.rmtree(dir_path)
+        os.makedirs(dir_path, exist_ok=True)
+
+        images_per_font = 300
+
+        for idx in range(images_per_font):
+            image = Image.new('RGB', (250, 100), (255, 255, 255))
+            draw = ImageDraw.Draw(image)
+
+            name = get_random_string()
+
+            x = random.randint(0, 50)
+            y = random.randint(0, 80)
+
+            draw.text((x, y), name, font=font, fill=(0, 0, 0))
+
+            image.save(f"{dir_path}/{name.replace(' ', '_')}.png")
